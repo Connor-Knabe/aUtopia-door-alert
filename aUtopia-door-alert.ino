ace3f92b3cc5f6a16878ff05ea23c331f6cb122b
Connor Knabe <ConnorKnabe@gmail.com>
2017-04-02 13:28:40 -0500

    Adding code

---
+++ aUtopia-door-alert.ino
@@ -0,0 +1,68 @@
+#define DOOR_SENSOR A0
+#define LED D7
+
+// time to wait before alarm switches states
+#define DEBOUNCE_SECONDS 300
+
+// whether or not door open is sensed: 0 = no, 1 = yes
+int opened = 0;
+
+// the alarm state: 0 = off, 1 = on
+int alarmState = 1;
+
+// the last time we switched alarm states
+int lastStateSwitchTime = 0;
+
+void setup() {
+
+    pinMode(LED, OUTPUT);
+    pinMode(DOOR_SENSOR, INPUT);
+
+    Particle.publish("online");
+
+    Particle.variable("opened", &opened, INT);
+
+    Particle.variable("alarmState", &alarmState, INT);
+}
+
+void loop() {
+    opened = hasBeenOpened();
+    if (opened) {
+        digitalWrite(LED, HIGH);
+
+
+        if (alarmState == 0) {
+            // only alarm if we're past the debounce interval
+            int now = Time.now();
+            if (now - lastStateSwitchTime > DEBOUNCE_SECONDS) {
+                alarmState = 1;
+                lastStateSwitchTime = now;
+                Particle.publish("aUtopia-door-alert-fc2", "on", 60, PRIVATE);
+            }
+        }
+    } else {
+        digitalWrite(LED, LOW);
+
+        //
+        // Alarm off
+        //
+        if (alarmState == 1) {
+            // only alarm if we're past the debounce interval
+            int now = Time.now();
+            if (now - lastStateSwitchTime > DEBOUNCE_SECONDS) {
+                alarmState = 0;
+                lastStateSwitchTime = now;
+                Particle.publish("aUtopia-door-alert-fc2", "off", 60, PRIVATE);
+            }
+        }
+    }
+}
+
+// determine if we're open or not
+boolean hasBeenOpened() {
+    if (digitalRead(DOOR_SENSOR) == LOW) {
+        return true;
+    } else {
+        return false;
+    }
+}
